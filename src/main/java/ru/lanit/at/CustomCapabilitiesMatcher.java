f8c651e4479d07fb926e0e0ea0bbc86e1699f7ef
lozhkin <Ugj*3,fYe92*&>
2021-01-25 09:48:23 +0300

    add device name cap

--- src/main/java/ru/lanit/at/CustomCapabilitiesMatcher.java
+++ src/main/java/ru/lanit/at/CustomCapabilitiesMatcher.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2019 Software Freedom Conservancy (SFC)
- * Modifications copyright (C) 2019 Inmarsat plc
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package ru.lanit.at;
 
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -28,27 +12,6 @@ import java.util.Map;
 
 import static org.openqa.selenium.remote.CapabilityType.BROWSER_NAME;
 
-/**
- * <p>A capabilities matcher based on the {@link org.openqa.grid.internal.utils.DefaultCapabilityMatcher}
- * implementation by seleniumHQ. It compares the {@link org.openqa.selenium.Capabilities} of the grid to
- * the {@link org.openqa.selenium.remote.DesiredCapabilities} requested. If the capabilities of both match
- * then it returns true and the node information will be returned by grid.</p>
- *
- * <p>Things are currently case sensitive.</p>
- *
- * <p>The {@link org.openqa.grid.internal.utils.DefaultCapabilityMatcher} and hence this class follow the
- * {@link Validator} pattern. The following validators are registered in this capabilities matcher:</p>
- *
- * <ul>
- *     <li>{@link AliasedPropertyValidator#AliasedPropertyValidator(String...)} {@link CapabilityType#BROWSER_NAME}</li>
- *     <li>{@link VersionValidator#VersionValidator(String)} {@link CapabilityType#BROWSER_VERSION}</li>
- *     <li>{@link FirefoxSpecificValidator}</li>
- *     <li>{@link PlatformValidator}</li>
- *     <li>{@link VersionValidator#VersionValidator(String)} {@link CapabilityType#VERSION}</li>
- *     <li>{@link SafariSpecificValidator}</li>
- *     <li>{@link SimplePropertyValidator#SimplePropertyValidator(String...)}</li>
- * </ul>
- */
 public class CustomCapabilitiesMatcher implements CapabilityMatcher {
 
     private final List<Validator> validators = new ArrayList<>();
@@ -56,27 +19,15 @@ public class CustomCapabilitiesMatcher implements CapabilityMatcher {
         validators.addAll(Arrays.asList(
             new AliasedPropertyValidator(BROWSER_NAME, "browser"),
             new VersionValidator(CapabilityType.BROWSER_VERSION),
-            new FirefoxSpecificValidator(),
             new PlatformValidator(),
             new VersionValidator(CapabilityType.VERSION),
             new VersionValidator(MobileCapabilityType.PLATFORM_VERSION),
-            new SafariSpecificValidator(),
             new SimplePropertyValidator(CapabilityType.APPLICATION_NAME),
-            new AppiumPropertyValidator(MobileCapabilityType.UDID)
+            new AppiumPropertyValidator(MobileCapabilityType.UDID),
+            new AppiumPropertyValidator(MobileCapabilityType.DEVICE_NAME)
         ));
     }
 
-    /**
-     * ENTRY POINT
-     *
-     * <p>This is the called function by the Grid.</p>
-     *
-     * @param providedCapabilities the {@link org.openqa.selenium.Capabilities} provided by the grid
-     * @param requestedCapabilities  the {@link org.openqa.selenium.remote.DesiredCapabilities} provided externally
-     *
-     * @return true if matches, false if not, will work off first one. The provided and requested capabilities cannot
-     * be null.
-     */
     @Override
     public boolean matches(Map<String, Object> providedCapabilities, Map<String, Object> requestedCapabilities) {
 
