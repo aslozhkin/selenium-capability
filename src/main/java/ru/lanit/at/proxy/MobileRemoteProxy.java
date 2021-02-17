86c1bc766c5f713dfc825cd67993755ad56cd035
aslozhkin <h#o345%$Ty*14O@q>
2021-02-17 09:20:05 +0300

    test afterSession

--- src/main/java/ru/lanit/at/proxy/MobileRemoteProxy.java
+++ src/main/java/ru/lanit/at/proxy/MobileRemoteProxy.java
@@ -6,6 +6,10 @@ import org.openqa.grid.internal.TestSession;
 import org.openqa.grid.selenium.proxy.DefaultRemoteProxy;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.logging.Logger;
 
 public class MobileRemoteProxy extends DefaultRemoteProxy {
@@ -17,13 +21,23 @@ public class MobileRemoteProxy extends DefaultRemoteProxy {
 
     @Override
     public void afterSession(TestSession session) {
-        LOGGER.info(session.getSlot().getRemoteURL().toString());
-
-//        String[] cmd = new String[]{"/bin/bash", "-c", "adb devices > testfile.txt"};
-//        try {
-//            Runtime.getRuntime().exec(cmd);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        URL url = null;
+        try {
+            url = new URL(session.getSlot().getRemoteURL().toString().replaceAll("/wd/hub", "") + ":3333");
+        } catch (MalformedURLException e) {
+            LOGGER.info("Не удалось получить адрес сессии");
+        }
+        HttpURLConnection con = null;
+        try {
+            con = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            LOGGER.info("Не удалось установить подключение с сессией: \"" + session.getSlot().getRemoteURL().toString() + "\"");
+        }
+        try {
+            con.setRequestMethod("GET");
+            LOGGER.info("Отправка запроса к узлу: \"" + session.getSlot().getRemoteURL().toString() + "\"");
+        } catch (ProtocolException e) {
+            LOGGER.info("Не удалось отправить GET запрос : \"" + session.getSlot().getRemoteURL().toString() + "\"");
+        }
     }
 }
